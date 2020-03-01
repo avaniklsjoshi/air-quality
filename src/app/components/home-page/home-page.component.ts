6268e470a41fb529bb1daf11db8b9c1bc60b6559
Avani Joshi <avani.joshi@etecture.de>
2020-03-01 23:29:29 +0100

    get attached

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import axios from 'axios';
 
 export interface City {
   id: number;
@@ -11,15 +11,22 @@ export interface City {
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit, OnDestroy {
-  title = 'Air Quality Data';
-  airData = [{"_id":"5e5531b21c9d4400006eff28","name":"Denmark","rank":1,"flag":"dk","airIndex":878,"population":32343},{"_id":"5e5531b21c9d4400006eff26","name":"Australia","rank":2,"flag":"au","airIndex":628,"population":3232243},{"_id":"5e5531b21c9d4400006eff25","name":"India","rank":3,"flag":"in","airIndex":80,"population":323233243},{"_id":"5e5531b21c9d4400006eff27","name":"Germany","rank":4,"flag":"de","airIndex":50,"population":33243}];
+  airData = [];
+  constructor() {
+    axios.get('http://localhost:4000/')
+      .then(response => {
+        // handle success
+        console.log(response.data);
+        this.airData = response.data;
+        this.ngOnInit();
+      })
+      .catch(error => {
+        // handle error
+        console.log(error);
+      });
+  }
 
-  constructor(
-  ) { }
-
-  // When this component is loaded, we'll call the dealService and get our public deals.
   ngOnInit() { }
-
   ngOnDestroy() {
   }
 }
