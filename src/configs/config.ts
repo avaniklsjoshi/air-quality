3daa4c7226da472281cd6ccff3b1423ff5e9d6d0
avaniklsjoshi <avaniklsjoshi@gmail.com>
2020-03-02 21:55:15 +0100

    Delete config.ts

--- src/configs/config.ts
+++
@@ -1,7 +0,0 @@
-export const API_BASE_URL = 'http://localhost:4000';
-export const AUTH_URL = 'https://avanijoshi.auth0.com/oauth/token';
-export const CLIENT_ID = 'A77tFavPl3L0PjFcfm7lz0Mv4uubP3QE';
-export const CLIENT_SECRET = '8aYvZnYCMSsxj8mU9mir1B7SH-K8u5ab5b9OtDjbNXN6GNvmKcPVBMcC-3qTbWz7';
-export const AUDIENCE = 'https://air-quality';
-export const GRANT_TYPE = 'client_credentials';
-
